ad35903ca465898a7cb53c84735d1bdc38b6bd9c
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-05-24 23:46:26 +0530

    changes1

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,8 @@ Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
     Route::patch('/update/{id}', [UserMetaController::class, 'update'])->name('update');
     Route::patch('/update/skills/{id}', [UserMetaController::class, 'updateOrCreateSkills'])->name('update.skills');
     Route::post('/get-languages', [UserMetaController::class, 'getLangauges'])->name('get-language');
-    Route::post('/get-countries', [UserMetaController::class, 'getCountries'])->name('get-language');
-    Route::post('/get-timezones', [UserMetaController::class, 'getTimezone'])->name('get-language');
+    Route::post('/get-countries', [UserMetaController::class, 'getCountries'])->name('get-country');
+    Route::post('/get-timezones', [UserMetaController::class, 'getTimezone'])->name('get-timzone');
 
 
 });
