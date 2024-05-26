4e62a8a85e3c8bff94f481867d1539e537f3eeae
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-05-26 18:10:19 +0530

    changes7

--- app/Models/UserMeta.php
+++ app/Models/UserMeta.php
@@ -10,4 +10,13 @@ class UserMeta extends Model
     use HasFactory;
 
     protected $guarded =[];
+
+    public function getProfilePicAttribute($value)
+    {
+        if ($value) {
+            // Prepend your AWS S3 bucket URL to the profile_pic path
+            return env('AWS_PATH') . $value;
+        }
+        return null;
+    }
 }
