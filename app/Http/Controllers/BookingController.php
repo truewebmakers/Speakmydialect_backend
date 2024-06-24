ce748173302d55694306ce7c06663badaf09e966
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-06-24 14:32:14 +0530

    asd11

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -75,7 +75,6 @@ class BookingController extends Controller
     public function updateClientStatus($id , $status ='')
     {
         $query = Booking::where(['id' => $id])->update(['work_status' => $status]);
-
         return response()->json(['message' => 'status Updated.' ,'data' =>$query ,'status' => true],200);
     }
 
