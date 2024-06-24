4c6eb262d8df958dbe411786adf2de9a0cee9d24
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-06-24 15:12:19 +0530

    client5

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -12,21 +12,21 @@ class Booking extends Model
 
     public function translator()
     {
-        return $this->belongsTo(User::class,'translator_id','id');
+        return $this->belongsTo(User::class,'translator_id','id')->select('uuid', 'fname', 'lname','email','user_type'); // Replace 'id', 'name', 'email' with the columns you need;
     }
     public function translatorMeta()
     {
-        return $this->belongsTo(UserMeta::class,'translator_id','user_id');
+        return $this->belongsTo(UserMeta::class,'translator_id','user_id')->select('phone', 'profile_pic', 'intro','gender'); // Replace 'id', 'name', 'email' with the columns you need;
     }
 
     public function client()
     {
-        return $this->belongsTo(User::class,'client_id','id');
+        return $this->belongsTo(User::class,'client_id','id')->select('uuid', 'fname', 'lname','email','user_type');
     }
 
     public function clientMeta()
     {
-        return $this->belongsTo(UserMeta::class,'client_id','user_id');
+        return $this->belongsTo(UserMeta::class,'client_id','user_id')->select('phone', 'profile_pic', 'intro','gender');
     }
 
 
