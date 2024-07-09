c8653ccb9a4af7cf81573f07917ad544b270c5f1
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-07-09 16:16:27 +0530

    changes23

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -70,6 +70,9 @@ class BookingController extends Controller
         if($request->input('type') == 'canceled_booking'){
             $query->where('status', $status);
         }
+        if($request->input('type') == 'rejected_booking'){
+            $query->where('status', $status);
+        }
 
 
         $booking = $query->orderBy('created_at','desc')->get();
