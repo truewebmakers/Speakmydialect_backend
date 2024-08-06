b9d1e7664f2ae7feada0ae4d67b57a3f50ec46dc
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-08-06 21:33:04 +0530

    invoice generator14

--- routes/api.php
+++ routes/api.php
@@ -94,3 +94,5 @@ Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
 
 });
 Route::post('/payout/charge', [PayoutController::class, 'createCharge'])->name('payout.crete');
+Route::get('/payout/invoices/{id}', [PayoutController::class, 'getInvoice'])->name('payout.invoice');
+
