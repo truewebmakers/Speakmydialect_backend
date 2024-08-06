d9b546d85b9a049782d41ccd5b7aa671297261cf
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-08-06 17:21:28 +0530

    fresh

--- database/migrations/2024_08_06_064530_create_payouts_table.php
+++ database/migrations/2024_08_06_064530_create_payouts_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->uuid('uuid')->nullable();
             $table->unsignedBigInteger('job_id');
             $table->foreign('job_id')->references('id')->on('bookings')->onDelete('cascade');
-            // bookings
             $table->string('stripe_id')->unique();
             $table->integer('amount')->default(0);
             $table->string('currency')->default('usd');
