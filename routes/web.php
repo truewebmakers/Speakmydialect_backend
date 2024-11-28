cab732a76c25eef5cb2b6585088467a4a148a14b
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-11-28 16:42:05 +0530

    adding reset

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/email/verify/{id}/{hash}', [VerificationController::class, 'verify'
 Route::get('/generate-invoice/{id}', [PayoutController::class, 'generateInvoice'])->name('view.invoice');
 
 
-Auth::routes(['reset' => true]);
+// Auth::routes(['reset' => true]);
 
 Route::get('/password/reset/{token}', [AuthController::class, 'showResetForm'])->name('password.reset');
 Route::post('/password/reset', [AuthController::class, 'reset'])->name('password.update');
