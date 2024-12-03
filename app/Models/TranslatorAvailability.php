67e2b9175be85546ed8521e1d7c3ef970880aff5
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-12-03 20:37:29 +0530

    get slotsas

--- app/Models/TranslatorAvailability.php
+++ app/Models/TranslatorAvailability.php
@@ -10,7 +10,6 @@ class TranslatorAvailability extends Model
     use HasFactory;
     protected $fillable = ['translator_id', 'day', 'start_time', 'end_time','is_enabled'];
 
-    // You can add relationships if needed
     public function translator()
     {
         return $this->belongsTo(User::class, 'translator_id');
